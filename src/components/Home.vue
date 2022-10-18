52375fa8e82784bd49445336de9b4595b155efc6
XinyuYang <y920459106@163.com>
2022-10-18 01:15:56 -0400

    Add unique product, trending products, brands section, page footer and copyright section. Edit homeyOffer images.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -30,7 +30,8 @@
       <el-menu-item index="6">Contact</el-menu-item>
       <el-menu-item class="searchArea">
         <el-input v-model="searchContent" placeholder="" class="searchInput"></el-input>
-        <el-button class="searchButton">
+        <el-button style="background-color: #e628a6;
+        position: relative; right: 6px; height: 74%; width: 25%;">
           <i class="el-icon-search searchIcon"></i>
         </el-button>
       </el-menu-item>
@@ -103,12 +104,174 @@
       <el-row>
         <el-col :span="5"><div class="grid-content"></div></el-col>
         <el-col :span="3" v-for="(o, index) in homeyOffers" :key="o" class="advCard">
-          <el-card :body-style="{ padding: '0px' }">
-            <el-image :src="homeyOffers[index].image"></el-image>
+          <el-card :body-style="{ padding: '0px'}">
+            <el-image :src="homeyOffers[index].image" style="width: 100%;height: 200px"></el-image>
           </el-card>
         </el-col>
       </el-row>
     </div>
+  </div>
+  <div class="unique">
+    <el-row>
+      <el-col :span="6"><div class="grid-content"></div></el-col>
+      <el-col :span="6">
+        <el-image :src="uniqueProduct.image"></el-image>
+      </el-col>
+      <el-col :span="8">
+        <h1>Unique Features of Latest & Trending Products</h1>
+        <ul>
+          <li v-for="(o, index) in uniqueProduct.details" :key="o">
+            {{ uniqueProduct.details[index] }}
+          </li>
+        </ul>
+        <el-row>
+          <el-col :span="6">
+            <el-button style="background-color: #e628a6; color: #FFFFFF">
+              Add To Cart
+            </el-button>
+          </el-col>
+          <el-col :span="8">
+            <el-row>{{ uniqueProduct.name }}</el-row>
+            <el-row>{{ uniqueProduct.price }}</el-row>
+          </el-col>
+        </el-row>
+      </el-col>
+    </el-row>
+  </div>
+  <div class="trendingProducts">
+    <h1>Trending Products</h1>
+    <el-row>
+      <el-col :span="3"><div class="grid-content"></div></el-col>
+      <el-col :span="4" v-for="(o, index) in 4" :key="o" class="advCard">
+        <el-card :body-style="{ padding: '5px' }">
+          <el-image :src="featured.image"></el-image>
+          <div class="featuredDetail">
+            <h4>{{ featured.name }}</h4>
+            <div>
+              <span>{{ latest.price }}</span>
+              <span style="text-decoration: line-through; color: darkgrey">{{ latest.originalPrice }}</span>
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="3"><div class="grid-content"></div></el-col>
+      <el-col :span="6">
+        <el-card class="trendCard1">
+          <h3>23% off in all products</h3>
+          <el-link style="color: #e628a6">Shop Now</el-link>
+          <el-image :src="trendCard1"  class="trendImg1"></el-image>
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card class="trendCard2">
+          <h3>23% off in all products</h3>
+          <el-link style="color: #e628a6">View Collection</el-link>
+          <el-image :src="trendCard2" class="trendImg2"></el-image>
+        </el-card>
+      </el-col>
+      <el-col :span="5" class="trendSide">
+        <el-row v-for="index in 3" :key="index" style="margin-bottom: 6%">
+          <el-col :span="8">
+            <el-image :src="trendImage" class="trendImage"></el-image>
+          </el-col>
+          <el-col :span="12">
+            <el-row style="float: left; font-weight: bolder">
+              Executive Seat Chair
+            </el-row>
+            <el-row style="float: left; text-decoration: line-through">
+              $32.00
+            </el-row>
+          </el-col>
+        </el-row>
+      </el-col>
+    </el-row>
+  </div>
+  <div class="brands">
+    <el-row>
+      <el-col :span="6"><div class="grid-content"></div></el-col>
+      <el-col :span="12">
+        <el-image :src="brandsImage"></el-image>
+      </el-col>
+    </el-row>
+  </div>
+  <div class="pageFooter">
+    <el-row>
+      <el-col :span="5"><div class="grid-content"></div></el-col>
+      <el-col :span="6">
+        <el-row>
+          <h1 style="float: left">Homey</h1>
+        </el-row>
+        <el-row>
+          <el-input placeholder="Enter Email Address" style="width: 60%; float: left;"></el-input>
+          <el-button style="float: left; width: 35%; position: relative; right: 12px;
+          background-color: #e628a6; color: #FFFFFF">
+            Sign up
+          </el-button>
+        </el-row>
+        <el-row>
+          <el-link style="float: left; margin-top: 5%">
+            Contact Info
+          </el-link>
+        </el-row>
+        <el-row>
+          <el-link style="float: left; margin-top: 5%">
+            1010 Commonwealth Avenue, Boston, MA 02215
+          </el-link>
+        </el-row>
+      </el-col>
+      <el-col :span="4" style="margin-left: 5%">
+        <el-row class="categories">
+          <h3>Categories</h3>
+        </el-row>
+        <el-row class="categories">
+          <el-link>Chairs & Sofas</el-link>
+        </el-row>
+        <el-row class="categories">
+          <el-link>Tables</el-link>
+        </el-row>
+        <el-row class="categories">
+          <el-link>Bed</el-link>
+        </el-row>
+        <el-row class="categories">
+          <el-link>Bookcases</el-link>
+        </el-row>
+        <el-row class="categories">
+          <el-link>Dressers & Wardrobes</el-link>
+        </el-row>
+      </el-col>
+      <el-col :span="4">
+        <el-row class="customerCare">
+          <h3>Customer Care</h3>
+        </el-row>
+        <el-row class="customerCare">
+          <el-link>My Account</el-link>
+        </el-row>
+        <el-row class="customerCare">
+          <el-link>Order History</el-link>
+        </el-row>
+        <el-row class="customerCare">
+          <el-link>Order Tracking</el-link>
+        </el-row>
+        <el-row class="customerCare">
+          <el-link>FAQ</el-link>
+        </el-row>
+        <el-row class="customerCare">
+          <el-link>Contact Us</el-link>
+        </el-row>
+      </el-col>
+    </el-row>
+  </div>
+  <div class="copyRight">
+    <el-row>
+      <el-col :span="6"><div style="min-height: 12px"></div></el-col>
+      <el-col :span="4" style="padding-top: 1%">©Homey - All Rights Reserved</el-col>
+      <el-col :span="6"><div style="min-height: 12px"></div></el-col>
+      <el-col :span="4">
+        <el-image :src="shareIcon" style="height: 50%; width: 50%;"></el-image>
+      </el-col>
+    </el-row>
   </div>
 </div>
 </template>
@@ -153,7 +316,22 @@ export default {
         {
           image: require('../assets/homeyOffer4.png')
         }
-      ]
+      ],
+      uniqueProduct: {
+        image: require('../assets/uniqueProduct.png'),
+        details: [
+          'All frames constructed with hardwood solids and laminates',
+          'Reinforced with wood dowels, glue, screw-nails corner blocks and machine nails',
+          'Arms, backs and seats are structurally reinforced'
+        ],
+        name: 'B&B Italian Sofa',
+        price: '$32'
+      },
+      trendCard1: require('../assets/trendCard1.png'),
+      trendCard2: require('../assets/trendCard2.png'),
+      trendImage: require('../assets/trendImage.png'),
+      brandsImage: require('../assets/brandsImage.png'),
+      shareIcon: require('../assets/shareIcon.png')
     }
   },
   methods: {
@@ -192,13 +370,6 @@ export default {
   padding-left: 5%;
   width: 25%;
 }
-.searchButton{
-  position: relative;
-  right: 6px;
-  height: 74%;
-  width: 25%;
-  background-color: #e628a6;
-}
 .searchInput{
   width: 80%;
 }
@@ -223,5 +394,69 @@ export default {
 }
 .latestItem{
   margin-right: 2%;
+}
+.unique{
+  margin-top: 5%;
+  margin-bottom: 5%;
+  padding-top: 5%;
+  min-height: 400px;
+  background-color: #f1f0fe;
+  text-align: left
+}
+.pageFooter{
+  padding-top: 3%;
+  padding-bottom: 6%;
+  margin-top: 5%;
+  background-color: #f1f0fe;
+}
+.trendCard1{
+  background-color: #fef6fb;
+  height: 200px;
+  margin-left: 8%;
+  margin-top: 10%;
+  text-align: left;
+}
+.trendCard2{
+  background-color: #efeffa;
+  height: 200px;
+  margin-left: 5%;
+  margin-top: 10%;
+  text-align: left;
+}
+.trendImg1{
+  width: 45%;
+  height: 45%;
+  float: right;
+}
+.trendImg2{
+  width: 80%;
+  height: 80%;
+  float: right;
+}
+.trendImage{
+  width: 85%;
+  height:85%;
+  float: left;
+}
+.trendSide{
+  margin-top: 2.5%;
+  margin-left: 1.3%;
+}
+.brands{
+  margin-top: 5%;
+}
+.categories{
+  margin-bottom: 2%;
+  text-align: left;
+}
+.customerCare{
+  margin-bottom: 2%;
+  text-align: left;
+}
+.copyRight{
+  padding: 1%;
+  background-color: #e6e4f6;
+  color: darkgrey;
+  font-weight: bold;
 }
 </style>
