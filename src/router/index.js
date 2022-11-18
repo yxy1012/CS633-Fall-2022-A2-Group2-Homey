9c4c74293b0916667bedd946856e607e99673f4e
XinyuYang <y920459106@163.com>
2022-11-18 10:59:15 -0500

    Login + Register

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import VueRouter from 'vue-router'
 import Vue from 'vue'
 import Home from '@/pages/Home'
 import Login from "@/pages/Login";
+import Register from "@/pages/Register";
+import register from "@/pages/Register";
 Vue.use(VueRouter)
 
 export default new VueRouter({
@@ -18,6 +20,13 @@ export default new VueRouter({
             component: Login,
             meta:{
                 title: 'login'
+            }
+        },
+        {
+            path: '/register',
+            component: Register,
+            meta:{
+                title: 'register'
             }
         }
     ]
