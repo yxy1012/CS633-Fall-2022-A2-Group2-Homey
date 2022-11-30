557b845f21da41ccaf74a805de153a244a26c9e1
snekajo <snekajo@gmail.com>
2022-11-30 14:52:59 -0500

    first commit

---
+++ src/main/java/com/group2/cs633fall2022a2group2homeybackend/Cs633Fall2022A2Group2HomeyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.group2.cs633fall2022a2group2homeybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs633Fall2022A2Group2HomeyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cs633Fall2022A2Group2HomeyBackendApplication.class, args);
+    }
+
+}
